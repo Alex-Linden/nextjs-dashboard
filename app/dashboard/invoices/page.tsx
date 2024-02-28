b86e25a3cffd25b0d246d16de3838d0586360fd8
Alex Linden <alextl2004@gmail.com>
2024-02-27 19:28:08 -0800

    remove facebook api

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,25 +1,3 @@
-import { useServer } from 'next/server';
-
 export default function Page() {
-  const serverData = useServer(() => {
-    // This function might run on the server side to fetch or compute data
-    return fetchSomeDataFromDatabase();
-  });
-
-  return <div>{serverData}</div>;
-}
-  // }
-
-
-
-
-  // function MyComponent() {
-  //   const serverData = useServer(() => {
-  //     // This function might run on the server side to fetch or compute data
-  //     return fetchSomeDataFromDatabase();
-  //   });
-
-  //   return <div>{serverData}</div>;
-  // }
-
-  // export default MyComponent;
+    return <p>Invoices Page</p>;
+  }
