ec18ae9ec80fa6b8e338370a2afb12dba7fedc99
Alex Linden <alextl2004@gmail.com>
2024-02-27 19:12:30 -0800

    testing use server

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,25 @@
+import { useServer } from 'next/server';
+
 export default function Page() {
-    return <p>Invoices Page</p>;
-  }
+  const serverData = useServer(() => {
+    // This function might run on the server side to fetch or compute data
+    return fetchSomeDataFromDatabase();
+  });
+
+  return <div>{serverData}</div>;
+}
+  // }
+
+
+
+
+  // function MyComponent() {
+  //   const serverData = useServer(() => {
+  //     // This function might run on the server side to fetch or compute data
+  //     return fetchSomeDataFromDatabase();
+  //   });
+
+  //   return <div>{serverData}</div>;
+  // }
+
+  // export default MyComponent;
